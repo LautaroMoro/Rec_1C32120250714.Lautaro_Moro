b4fc4022a72999f4f781f8a65bb3dbc199b90736
Lautaro Moro <127876731+LautaroMoro@users.noreply.github.com>
2025-07-14 18:22:02 -0300

    recu parcial 14/07

---
+++ src/rec_1c32120250714/lautaro_moro/Rec_1C32120250714Lautaro_Moro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rec_1c32120250714.lautaro_moro;
+
+/**
+ *
+ * @author Lautaro
+ */
+public class Rec_1C32120250714Lautaro_Moro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
